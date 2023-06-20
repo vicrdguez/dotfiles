835557d044ef09a58dd812654bed7893d23570af
vicrdguez <vicrerdgz@gmail.com>
2023-06-20 18:45:48 +0400

    got into wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,60 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+local function scheme_for_appearance(appearance)
+  if appearance:find "Dark" then
+    return "Catppuccin Mocha"
+  else
+    return "Catppuccin Latte"
+  end
+end
+
+-- This is where you actually apply your config choices
+
+-- [[appearance
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_padding = {
+    top = 4,
+    bottom = 4,
+    left = 4,
+    right = 4
+}
+config.window_background_opacity = 1
+
+-- Fonts
+-- config.font = wezterm.font("Iosevka Term")
+config.font = wezterm.font("Iosevka Term", {
+    weight = "Medium",
+    -- stretch = "SemiExpanded"
+})
+config.font_size = 16
+-- ]]
+
+-- https://wezfurlong.org/wezterm/config/lua/keyassignment/OpenLinkAtMouseCursor.html
+config.mouse_bindings = {
+  -- Ctrl-click will open the link under the mouse cursor
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'CTRL',
+    action = wezterm.action.OpenLinkAtMouseCursor,
+  },
+}
+
+-- Using fish shell
+-- Spawn a fish shell in login mode
+config.default_prog = { '/usr/local/bin/fish', '-l' }
+
+-- and finally, return the configuration to wezterm
+return config
+
