e60f79aa75f3ab960e8d13d8bbaea5d21b429413
vicrdguez <vicrerdgz@gmail.com>
2023-07-08 11:42:48 +0400

    oil.nvim config and remaps

--- dot_config/nvim/lua/plugins/file.lua
+++ dot_config/nvim/lua/plugins/file.lua
@@ -1,71 +1,97 @@
 return {
     {
         'stevearc/oil.nvim',
-        opts = {},
         -- Optional dependencies
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        keys = {
-                {"<leader>f.", "<cmd>Oil<cr>", desc = "Open Oil browser"}
+        lazy = false,
+        opts = {
+            default_file_explorer = true,
+            columns = {
+                "permissions",
+                "size",
+                "icon"
+            },
+            view_options = {
+                show_hidden = true
             }
         },
-        {
-            "nvim-neo-tree/neo-tree.nvim",
-            branch = "v2.x",
-            cmd = "Neotree",
-            dependencies = {
-                "MunifTanjim/nui.nvim"
+        keys = {
+            {
+                "<leader>f.",
+                function() require("oil").open() end,
+                desc = "Open Oil browser"
             },
-            keys = {
-                {
-                    "<leader>fE",
-                    function()
-                        require("neo-tree.command").execute({toggle = true, dir = require("custom.utils").get_root(), position = "right" })
-                    end,
-                    desc = "Open neotree (root dir)"
-                },
-                {
-                    "<leader>fe",
-                    function()
-                        require("neo-tree.command").execute({toggle = true, dir = vim.loop.cwd(), position = "right"})
-                    end,
-                    desc = "Open neotree (cwd)"
-                },
-                {
-                    "<leader>e",
-                    function()
-                        require("neo-tree.command").execute({ action = "focus", dir = vim.loop.cwd(), position = "right" })
-                    end,
-                    desc = "Focus on neotree (cwd)"
-                }
-            },
-            opts = {
-                filesystem = {
-                    bind_to_cwd = false,
-                    follow_current_file = true
-                },
-                default_component_configs = {
-                    icon = {
-                        folder_empty = "󰜌",
-                        folder_empty_open = "󰜌",
-                    },
-                    git_status = {
-                        symbols = {
-                            -- Change type
-                            added     = "✚", -- or "✚", but this is redundant info if you use git_status_colors on the name
-                            modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
-                            deleted   = "✖",-- this can only be used in the git_status source
-                            renamed   = "",-- this can only be used in the git_status source
-                            -- Status type
-                            untracked = "",
-                            ignored   = "",
-                            unstaged  = "",
-                            staged    = "",
-                            conflict  = "",
-                        },
-                    },
-                },
+            {
+                "<leader>ff",
+                function() require("oil").open_float() end,
+                desc = "Open Oil browser"
             }
-
+        },
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        cmd = "Neotree",
+        enabled = false,
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            'nvim-lua/plenary.nvim'
+        },
+        keys = {
+            {
+                "<leader>fE",
+                function()
+                    require("neo-tree.command").execute({
+                        toggle = true,
+                        dir = require("custom.utils").get_root(),
+                        position = "right"
+                    })
+                end,
+                desc = "Open neotree (root dir)"
+            },
+            {
+                "<leader>fe",
+                function()
+                    require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd(), position = "right" })
+                end,
+                desc = "Open neotree (cwd)"
+            },
+            {
+                "<leader>e",
+                function()
+                    require("neo-tree.command").execute({ action = "focus", dir = vim.loop.cwd(), position = "right" })
+                end,
+                desc = "Focus on neotree (cwd)"
+            }
+        },
+        opts = {
+            filesystem = {
+                bind_to_cwd = false,
+                follow_current_file = true
+            },
+            default_component_configs = {
+                icon = {
+                    folder_empty = "󰜌",
+                    folder_empty_open = "󰜌",
+                },
+                git_status = {
+                    symbols = {
+                        -- Change type
+                        added     = "✚", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                        modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                        deleted   = "✖", -- this can only be used in the git_status source
+                        renamed   = "", -- this can only be used in the git_status source
+                        -- Status type
+                        untracked = "",
+                        ignored   = "",
+                        unstaged  = "",
+                        staged    = "",
+                        conflict  = "",
+                    },
+                },
+            },
         }
 
     }
+
+}
